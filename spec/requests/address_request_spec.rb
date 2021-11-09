c53b2f8679091c28ca4a7518335b8fc15e608ffe
Elliot Olbright <elliotolbright@gmail.com>
2021-11-09 12:26:36 -0700

    update address request to create is address

--- spec/requests/address_request_spec.rb
+++ spec/requests/address_request_spec.rb
@@ -14,9 +14,10 @@ RSpec.describe 'Address API' do
       street: "1234 dope street",
       city: "San Francisco",
       state: "CA",
-      zipcode: "12345",
+      zipcode: "12345"
     )
   end
+
   it 'can return a users address' do 
     get "/api/v1/users/#{@user.id}/addresses/#{@address.id}"
 
@@ -42,5 +43,39 @@ RSpec.describe 'Address API' do
     expect(address[:data][0][:attributes][:state]).to be_a(String)
     expect(address[:data][0][:attributes]).to have_key(:zipcode)
     expect(address[:data][0][:attributes][:zipcode]).to be_a(String)
+  end 
+
+  it 'can create a users address' do 
+    body = { 
+      street: "222 high street",
+      city: "Denver",
+      state: "CO",
+      zipcode: "80222",
+     }
+
+    post "/api/v1/users/#{@user.id}/addresses", params: body, as: :json
+
+    expect(response).to be_successful
+    expect(response.status).to eq (200)
+
+    address = JSON.parse(response.body, symbolize_names: true)
+
+    expect(address).to have_key(:data)
+    expect(address[:data].count).to eq(3)
+    expect(address[:data]).to have_key(:id)
+    expect(address[:data][:id]).to be_a(String)
+    expect(address[:data]).to have_key(:type)
+    expect(address[:data][:type]).to eq("address")
+    expect(address[:data]).to have_key(:attributes)
+    expect(address[:data][:attributes]).to be_a(Hash)
+    expect(address[:data][:attributes].keys.count).to eq(4)
+    expect(address[:data][:attributes]).to have_key(:street)
+    expect(address[:data][:attributes][:street]).to be_a(String)
+    expect(address[:data][:attributes]).to have_key(:city)
+    expect(address[:data][:attributes][:city]).to be_a(String)
+    expect(address[:data][:attributes]).to have_key(:state)
+    expect(address[:data][:attributes][:state]).to be_a(String)
+    expect(address[:data][:attributes]).to have_key(:zipcode)
+    expect(address[:data][:attributes][:zipcode]).to be_a(String)
   end 
 end 
