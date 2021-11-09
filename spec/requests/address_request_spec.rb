3eda0d53bf703955a15ffe73399e2dbd6567a285
Elliot Olbright <elliotolbright@gmail.com>
2021-11-09 10:49:16 -0700

    update address request spec

--- spec/requests/address_request_spec.rb
+++ spec/requests/address_request_spec.rb
@@ -26,5 +26,21 @@ RSpec.describe 'Address API' do
     address = JSON.parse(response.body, symbolize_names: true)
 
     expect(address).to have_key(:data)
+    expect(address[:data].count).to eq(1)
+    expect(address[:data][0]).to have_key(:id)
+    expect(address[:data][0][:id]).to be_a(String)
+    expect(address[:data][0]).to have_key(:type)
+    expect(address[:data][0][:type]).to eq("address")
+    expect(address[:data][0]).to have_key(:attributes)
+    expect(address[:data][0][:attributes]).to be_a(Hash)
+    expect(address[:data][0][:attributes].keys.count).to eq(4)
+    expect(address[:data][0][:attributes]).to have_key(:street)
+    expect(address[:data][0][:attributes][:street]).to be_a(String)
+    expect(address[:data][0][:attributes]).to have_key(:city)
+    expect(address[:data][0][:attributes][:city]).to be_a(String)
+    expect(address[:data][0][:attributes]).to have_key(:state)
+    expect(address[:data][0][:attributes][:state]).to be_a(String)
+    expect(address[:data][0][:attributes]).to have_key(:zipcode)
+    expect(address[:data][0][:attributes][:zipcode]).to be_a(String)
   end 
 end 
