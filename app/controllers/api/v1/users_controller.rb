01985ba8614397ab901b138e0c5efee49b73f1cb
Elliot Olbright <elliotolbright@gmail.com>
2021-11-09 12:26:58 -0700

    add create to address controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,8 +4,24 @@ class Api::V1::UsersController < ApplicationController
     render json: UserSerializer.new(user)
   end 
 
+  def create 
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user)
+    else 
+      render json: { error: "Please fill in all" }, status: :not_found
+    end 
+  end
+
   private
   def user_params
-    params.permit(:id)
+    params.permit(
+      :id,
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end 
