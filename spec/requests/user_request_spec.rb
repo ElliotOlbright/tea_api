107ce622395c22c5f62853762ad721fd270bac36
Elliot Olbright <elliotolbright@gmail.com>
2021-11-09 12:25:18 -0700

    update user request spec to have create

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -36,4 +36,37 @@ RSpec.describe 'users API' do
     expect(user_info[:data][:attributes]).to have_key(:email)
     expect(user_info[:data][:attributes][:email]).to be_a(String)
   end 
+
+  it 'creates a user' do
+
+    body = { 
+      first_name: "elliot",
+      last_name: "olbright",
+      email: "elliotolbright@gmail.com",
+      password: "jimmy",
+      password_confirmation: "jimmy"
+     }
+
+    post "/api/v1/users", params: body, as: :json
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    user_info = JSON.parse(response.body, symbolize_names: true)
+    expect(user_info).to have_key(:data)
+    expect(user_info[:data]).to be_a(Hash)
+    expect(user_info[:data].keys.count).to eq(3)
+    expect(user_info[:data]).to have_key(:id)
+    expect(user_info[:data][:id]).to be_a(String)
+    expect(user_info[:data]).to have_key(:type)
+    expect(user_info[:data][:type]).to eq("user")
+    expect(user_info[:data]).to have_key(:attributes)
+    expect(user_info[:data][:attributes]).to be_a(Hash)
+    expect(user_info[:data][:attributes].keys.count).to eq(4)
+    expect(user_info[:data][:attributes]).to have_key(:first_name)
+    expect(user_info[:data][:attributes][:first_name]).to be_a(String)
+    expect(user_info[:data][:attributes]).to have_key(:last_name)
+    expect(user_info[:data][:attributes][:last_name]).to be_a(String)
+    expect(user_info[:data][:attributes]).to have_key(:email)
+    expect(user_info[:data][:attributes][:email]).to be_a(String)
+  end 
 end 
