925754cb007efd555b3dddde14f30b597f113b2b
Elliot Olbright <elliotolbright@gmail.com>
2021-11-09 12:26:47 -0700

    add create to user comntroller

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -1,16 +1,30 @@
 class Api::V1::AddressesController < ApplicationController
+
   def show 
     user = User.find_by(id: address_params[:user_id].to_i)
     address = user.addresses
     render json: AddressSerializer.new(address)
   end 
 
+  def create  
+    user = User.find_by(id: params[:user_id].to_i)
+    address = user.addresses.new(address_params)
+    if address.save
+      render json: AddressSerializer.new(address)
+    else
+      render json: { error: "An existing User id is required" }, status: :not_found
+    end 
+  end 
+  
   private
 
   def address_params
     params.permit(
-      :id,
-      :user_id
+      :user_id,
+      :street,
+      :city,
+      :state,
+      :zipcode
     )
   end
 end 
