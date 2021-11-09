d04fc2036fddc113a2b35959598b44fafb0d263b
Elliot Olbright <elliotolbright@gmail.com>
2021-11-09 13:22:53 -0700

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_194011) do
+ActiveRecord::Schema.define(version: 2021_11_09_194509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_194011) do
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
     t.integer "price"
-    t.integer "status"
+    t.integer "status", default: 0
     t.integer "frequency"
     t.bigint "user_id"
     t.datetime "created_at", null: false
